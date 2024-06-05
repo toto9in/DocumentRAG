a28459977741bfe80fa4429f8f195fe2c083a8e6
antonio <aaba03@gmail.com>
2024-06-04 21:06:07 -0300

    initial imp upload pdf

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Path, UploadFile
+from os import path, mkdir
+import uuid
 
 app = FastAPI()
 
@@ -10,4 +13,20 @@ def hello():
 @app.get("/query")
 def query_index():
     global index
-    
+
+## rota para pegar um documento específico, nao implementada 100%
+@app.get("/document/{document_id}")
+def get_document(document_id: Annotated[int, Path(title="The ID of the document to get", gt=0)]):
+    return {"document_id": document_id}
+
+## endpoint to save document in contracts folder, change status return later and return object with id
+@app.post("/document")
+async def upload_document(file: UploadFile):
+    try:
+        contract_id = uuid.uuid1()
+        file.filename = f"{contract_id}.pdf"
+        with open(f"contracts/{file.filename}", "wb+") as f:
+            f.write(file.file.read())
+        return {"filename": file.filename, "id": contract_id }
+    except Exception as e:
+        return {"error": str(e)}
